5d6c95723db0015ac73d32aa22e8bdd6e5b85366
wensf <772148609@qq.com>
2018-07-05 23:46:23 +0800

    define the LovelyRTOS data type

---
+++ Application/RTOS/OSType.h
@@ -0,0 +1,14 @@
+#ifndef __OSTYPE_H__
+#define __OSTYPE_H__
+
+#include <stdio.h>
+#include <string.h>
+
+typedef int LRT_RET;
+
+#define LRT_OK             0
+#define LRT_ERR           -1
+
+#define SIZEOF(x)          (sizeof(x)/sizeof(x[0]))
+
+#endif
