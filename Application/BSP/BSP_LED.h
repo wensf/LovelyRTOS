31288deec2de969071e42e9bd471713f57288d8c
wensf <772148609@qq.com>
2018-07-05 23:40:44 +0800

    add group BSP to place the low level driver files

---
+++ Application/BSP/BSP_LED.h
@@ -0,0 +1,23 @@
+#ifndef __BSP_LED_H__
+#define __BSP_LED_H__
+
+#define LED1_PIN            GPIO_Pin_7       
+#define LED1_PORT           GPIOG                   
+#define LED1_CLK            RCC_AHB1Periph_GPIOG	  
+
+#define LED2_PIN            GPIO_Pin_12                      
+#define LED2_PORT           GPIOD                       
+#define LED2_CLK            RCC_AHB1Periph_GPIOD    
+
+/*---------------------- LED Pin Definition ------------------------*/
+					
+#define LED1_ON() 	  		GPIO_ResetBits(LED1_PORT,LED1_PIN);	 
+#define LED1_OFF() 	 	 	GPIO_SetBits(LED1_PORT,LED1_PIN);		 
+
+#define LED2_ON 	  		GPIO_ResetBits(LED2_PORT,LED2_PIN);	 
+#define LED2_OFF 	  		GPIO_SetBits(LED2_PORT,LED2_PIN);
+
+extern void LED_GPIO_Init(void);
+extern void LED_set( int enable );
+
+#endif
