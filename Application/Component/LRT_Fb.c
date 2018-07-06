853a7139e54b6c94cdee739d4c71f19db73bc2c1
wensf <772148609@qq.com>
2018-07-07 00:53:01 +0800

    add fb files

---
+++ Application/Component/LRT_Fb.c
@@ -0,0 +1,8 @@
+#include "OSType.h"
+#include "LRT_Fb.h"
+
+int LRT_Fb_Init(void)
+{
+	return LRT_OK;
+}
+
---
+++ Application/Component/LRT_Fb.h
@@ -0,0 +1,6 @@
+#ifndef __LRT_FB_H__
+#define __LRT_FB_H__
+
+extern int LRT_Fb_Init(void);
+
+#endif
