d3dfee292990fd846e0891b02200a624c3aa6192
wensf <772148609@qq.com>
2018-07-07 00:45:07 +0800

    add the Input subsystem and frame buffer subsystem formwork

---
+++ Application/Component/LRT_Input.c
@@ -0,0 +1,14 @@
+/**
+ * File LRT_Input.c
+ * Implement the Input Subsystem
+ * V1.0
+ * 2018.07.07
+ */
+ 
+#include "OSType.h"
+#include "LRT_Input.h"
+
+int LRT_Input_Init(void)
+{
+	return LRT_OK;
+}
---
+++ Application/Component/LRT_Input.h
@@ -0,0 +1,13 @@
+/**
+ * File LRT_Input.h
+ * Implement the Input Subsystem
+ * V1.0
+ * 2018.07.07
+ */
+ 
+#ifndef __LRT_INPUT_H__
+#define __LRT_INPUT_H__
+
+extern int LRT_Input_Init(void);
+
+#endif
